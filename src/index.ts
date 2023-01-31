654e0f4781eb431fca947304f669860232073732
Hao Chen <hche8093@uni.sydney.edu.au>
2023-02-01 10:27:06 +1100

    Clean code

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,8 @@ import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 
 const user = new User();
-console.log(user);
-
 const company = new Company();
-console.log(company);
-
 const customMap = new CustomMap("map");
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+
+customMap.addMarker(user);
+customMap.addMarker(company);
